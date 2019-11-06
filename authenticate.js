865dfd91779ed9deaf2d4e6ae2ccb98342b6bc8a
dush1729 <dush1729@gmail.com>
2019-11-06 22:10:37 +0530

    use axios instead of request

--- authenticate.js
+++ authenticate.js
@@ -1,27 +1,20 @@
-var request = require('request');
+var axios = require('axios');
 var data = null
 var lastFetched = null
 
 module.exports.getBearer = function (callback) {
     var time = new Date().getTime() / 1000
     if (!lastFetched || time - lastFetched >= data["result"]["data"]["expires in"]) {
-        request.post(
-            "https://api.codechef.com/oauth/token",
-            { json: { "grant_type": "client_credentials", "scope": "public", "client_id": process.env.CLIEND_ID, "client_secret": process.env.CLIENT_SECRET } },
-            function (error, response, body) {
-                if (error) {
-                    callback(error, null)
-                }
-                else if (response.statusCode == 200) {
-                    data = body
-                    lastFetched = new Date().getTime() / 1000
-                    callback(null, body)
-                }
-                else {
-                    callback(new Error("API error: " + response.statusCode), null)
-                }
-            }
-        );
+        axios.post("https://api.codechef.com/oauth/token",
+            { "grant_type": "client_credentials", "scope": "public", "client_id": process.env.CLIEND_ID, "client_secret": process.env.CLIENT_SECRET })
+            .then(function (response) {
+                data = response.data
+                lastFetched = new Date().getTime() / 1000
+                callback(null, response.data)
+            })
+            .catch(function (error) {
+                callback(error, null)
+            });
     } else {
         callback(data, null)
     }
